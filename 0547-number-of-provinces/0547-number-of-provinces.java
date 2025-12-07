124da63934dca40b3af2681ba6f01b445180cf47
Sushant2245 <guptasushant426@gmail.com>
2025-12-07 14:35:32 +0530

    Time: 4 ms (20.15%), Space: 49.2 MB (10.56%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,35 +1,36 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n= isConnected.length;
-       ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
-       for(int i=0;i<n;i++){
-        adj.add(new ArrayList<>());
-       }
-       for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(isConnected[i][j]==1 && i!=j){
-                adj.get(i).add(j);
-                
+        ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
+        int n=isConnected.length;
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
             }
         }
-       }
-       int visited[]=new int[n];
-       int count=0;
-       for(int i=0;i<n;i++){
-        if(visited[i]==0){
-            count++;
-            dfs(i,visited,adj);
-        }
-       }
-       return count;
-    }
-    public void dfs(int n,int[]visited,ArrayList<ArrayList<Integer>> adj){
-        visited[n]=1;
-        for(Integer i: adj.get(n)){
+        int visited[]= new int[adj.size()];
+        int count=0;
+        for(int i=0;i<adj.size();i++){
             if(visited[i]==0){
-                dfs(i,visited,adj);
+                count++;
+                dfs(i,adj,visited);
             }
         }
 
+      return count;
     }
+    public void dfs(int node, ArrayList<ArrayList<Integer>> adj,int[]visited){
+        for(int i:adj.get(node)){
+            if(visited[i]==0){
+                visited[i]=1;
+                dfs(i,adj,visited);
+            }
+        }
+    }
+
 }
