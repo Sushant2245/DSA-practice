5eaab4c182ee8d353ddf80eb85a840a6b0fd243f
Sushant2245 <guptasushant426@gmail.com>
2025-12-07 16:46:02 +0530

    Time: 2 ms (81.78%), Space: 47.8 MB (32.57%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,34 +1,27 @@
 class Solution {
     public void solve(char[][] board) {
-        int m=board.length;
-        int n=board[0].length;
-        for(int i=0;i<m;i++){
-            if(board[i][0]=='O'){
-                dfs(board,i,0);
-            }
-            if(board[i][n-1]=='O'){
-                dfs(board,i,n-1);
-            }
+        int n=board.length;
+        int m=board[0].length;
+        for(int i=0;i<n;i++){
+            if(board[i][0]=='O') dfs(board,i,0);
+            if(board[i][m-1]=='O') dfs(board,i,m-1);
         }
-        for(int j=0;j<n;j++){
-            if(board[0][j]=='O'){
-                dfs(board,0,j);
-            }
-            if(board[m-1][j]=='O'){
-                dfs(board,m-1,j);
-            }
+        for(int j=0;j<m;j++){
+            if(board[0][j]=='O') dfs(board,0,j);
+            if(board[n-1][j]=='O')dfs(board,n-1,j);
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(board[i][j]=='O') board[i][j]='X';
                 if(board[i][j]=='s') board[i][j]='O';
+                
             }
         }
     }
-    public void dfs(char[][]board,int i,int j){
-        int m=board.length;
-        int n=board[0].length;
-        if(i<0 ||j<0 || i>=m ||j>=n || board[i][j]!='O') return;
+    public void dfs(char[][] board,int i,int j){
+        int n=board.length;
+        int m=board[0].length;
+        if(i>=n || j>=m || i<0 || j<0 || board[i][j]!='O') return;
         board[i][j]='s';
         dfs(board,i+1,j);
         dfs(board,i-1,j);
