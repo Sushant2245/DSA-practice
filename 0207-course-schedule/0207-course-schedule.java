9fb5eb945ee57d1163b8220fdde4da740821da38
Sushant2245 <guptasushant426@gmail.com>
2026-01-05 15:09:56 +0530

    Time: 7 ms (63.12%), Space: 47.6 MB (7.84%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,35 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int n=prerequisites.length;
         ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
-        int n=numCourses;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        for (int i = 0; i < prerequisites.length; i++) {
-           int a = prerequisites[i][0];
-           int b = prerequisites[i][1];
-            adj.get(b).add(a);
+        for(int[] e: prerequisites){
+            int u=e[0];
+            int v=e[1];
+            adj.get(u).add(v);
         }
-        //detect cycle using topo sort
-        int inDegree[]= new int[n];
-        for(int i=0;i<n ;i++){
+        Queue<Integer> q= new LinkedList<>();
+        int inDegree[]=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
             for(int it:adj.get(i)){
                 inDegree[it]++;
             }
         }
-        Queue<Integer> q= new LinkedList<>();
-        for(int i=0;i<inDegree.length;i++){
-            if(inDegree[i]==0){
-                q.offer(i);
+        for(int i=0;i<numCourses;i++){
+            if(inDegree[i]==0)q.offer(i);
+        }
+        ArrayList<Integer> topo=new ArrayList<>();
+        while(!q.isEmpty()){
+            int current=q.poll();
+            topo.add(current);
+            for(int it:adj.get(current)){
+                inDegree[it]--;
+                if(inDegree[it]==0)q.offer(it);
             }
         }
-       List<Integer> topo= new ArrayList<>();
-       while(!q.isEmpty()){
-        int node= q.poll();
-        topo.add(node);
-         for(int it: adj.get(node)){
-            inDegree[it]--;
-            if(inDegree[it]==0)q.offer(it);
-         }
-       }
-       if(topo.size()==n)return true;
-       return false;
+        if(topo.size()==numCourses)return true;
+        return false;
     }
 }
