1d2c2a1394245217e7fa27411e62f7b14f030739
Sushant2245 <guptasushant426@gmail.com>
2026-03-12 20:44:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans= new ArrayList<>();
+        helper(n,"",ans,0,0);
+        return ans;
+
+    }
+    public void helper(int n,String curr,List<String> ans,int open,int close){
+        if(curr.length()==2*n){
+            ans.add(curr);
+            return;
+        }
+        if(open<n){
+            helper(n,curr+'(',ans,open+1,close);
+        }
+        if(close<open){
+            helper(n,curr+')',ans,open,close+1);
+        }
+
+    }
+}
