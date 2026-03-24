a0c8e56627d88b5d2ef118ba1b75843759ca4603
Sushant2245 <guptasushant426@gmail.com>
2026-03-24 12:44:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -2,24 +2,51 @@ class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n=grid.length;
         if(grid[0][0]==1 || grid[n-1][n-1]==1)return -1;
-        int dir[][]={{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,-1},{1,-1},{-1,1}};
         Queue<int[]> q= new LinkedList<>();
-        q.offer(new int[]{0,0});
+        int dist=1;
         grid[0][0]=1;
+        q.offer(new int[]{0,0});
         while(!q.isEmpty()){
-            int[] curr= q.poll();
-            int r=curr[0];
-            int c=curr[1];
-            int dist=grid[r][c];
-            if(r==n-1 && c==n-1) return dist;
-            for(int it[]:dir){
-                int nr=r+it[0];
-                int nc=c+it[1];
-                if(nr>=0 && nc>=0 && nr<n && nc<n && grid[nr][nc]==0){
-                    q.offer(new int[]{nr,nc});
-                    grid[nr][nc]=dist+1;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int[] cur=q.poll();
+                int r=cur[0];
+                int c=cur[1];
+                if(r==n-1 && c==n-1) return dist;
+                if(r-1>=0 && grid[r-1][c]==0){
+                    q.offer(new int[]{r-1,c});
+                    grid[r-1][c]=1;
+                }
+                 if(r+1<n && grid[r+1][c]==0){
+                    q.offer(new int[]{r+1,c});
+                    grid[r+1][c]=1;
+                }
+                 if(c-1>=0 && grid[r][c-1]==0){
+                    q.offer(new int[]{r,c-1});
+                    grid[r][c-1]=1;
+                }
+                 if(c+1<n && grid[r][c+1]==0){
+                    q.offer(new int[]{r,c+1});
+                    grid[r][c+1]=1;
+                }
+                 if(r-1>=0 && c-1>=0 && grid[r-1][c-1]==0){
+                    q.offer(new int[]{r-1,c-1});
+                    grid[r-1][c-1]=1;
+                }
+                if(r+1<n && c+1<n && grid[r+1][c+1]==0){
+                    q.offer(new int[]{r+1,c+1});
+                    grid[r+1][c+1]=1;
+                }
+                if(r-1>=0 && c+1<n && grid[r-1][c+1]==0){
+                    q.offer(new int[]{r-1,c+1});
+                    grid[r-1][c+1]=1;
+                }
+                if(r+1<n && c-1>=0 && grid[r+1][c-1]==0){
+                    q.offer(new int[]{r+1,c-1});
+                    grid[r+1][c-1]=1;
                 }
             }
+            dist++;
         }
         return -1;
     }
