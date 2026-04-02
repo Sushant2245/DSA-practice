55d568de5d2dc549cc31af907bbdb1764d5e3b41
Sushant2245 <guptasushant426@gmail.com>
2026-04-02 13:16:55 +0530

    Time: 0 ms (100%), Space: 44.3 MB (47.02%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -5,12 +5,12 @@ class Solution {
         while(s<e){
             int mid=s+(e-s)/2;
             if(nums[mid]>nums[mid+1]){
-                return mid;
+                e=mid;
             }
             else{
                 s=mid+1;
             }
         }
-        return -1;
+        return s;
     }
 }
