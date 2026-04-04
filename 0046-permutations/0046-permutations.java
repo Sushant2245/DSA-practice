795ac7d6564e9cb9430efa14e9513dd9c23cffdd
Sushant2245 <guptasushant426@gmail.com>
2026-04-04 12:20:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,24 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         int taken[]=new int[nums.length];
-        List<List<Integer>> ans= new ArrayList<>();
-        List<Integer> ls=new ArrayList<>();
-        helper(taken,ans,ls,nums);
-        return ans;
-
+        List<List<Integer>> result=new ArrayList<>();
+        List<Integer> current=new ArrayList<>();
+        helper(nums,taken,result,current);
+        return result;
     }
-    public void helper(int[] taken, List<List<Integer>> ans,List<Integer> ls,int[] nums){
-        if(ls.size()==nums.length){
-            ans.add(new ArrayList<>(ls));
+    public void helper(int[] nums,int[] taken, List<List<Integer>> result, List<Integer> current){
+        if(current.size()==nums.length){
+            result.add(new ArrayList<>(current));
             return;
         }
-       for(int i=0;i<nums.length;i++){
-        if(taken[i]==1)continue;
-        taken[i]=1;
-        ls.add(nums[i]);
-        helper(taken,ans,ls,nums);
-        ls.remove(ls.size()-1);
-        taken[i]=0;
+        for(int i=0;i<nums.length;i++){
+            if(taken[i]==1)continue;
+            taken[i]=1;
+            current.add(nums[i]);
+            helper(nums,taken,result,current);
+            current.remove(current.size()-1);
+            taken[i]=0;
 
-       }
-
+        }
     }
 }
