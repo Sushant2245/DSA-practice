39aa4ee3489d98f9bc9f4fe1b0ace1d2bbf706b2
Sushant2245 <guptasushant426@gmail.com>
2026-04-04 14:44:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,26 +1,29 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int n=board.length;
-        int m=board[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int m= board.length;
+        int n=board[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(board[i][j]==word.charAt(0)){
-                   if(dfs(board,word,i,j,n,m,0))return true;
+                    if(dfs(board,word,0,i,j))return true;
                 }
             }
         }
         return false;
     }
-    public boolean dfs(char[][] board, String word,int i,int j,int n,int m,int index){
+    public boolean dfs(char[][] board, String word, int index,int i,int j){
+        int m=board.length;
+        int n=board[0].length;
         if(index==word.length())return true;
-        if(i<0 || j<0 || i>=n || j>=m || board[i][j]!=word.charAt(index) || board[i][j]=='.' )return false;
+        if(i<0 || j<0||i>=m || j>=n || board[i][j]!=word.charAt(index)|| board[i][j]=='.')return false;
         char temp=board[i][j];
         board[i][j]='.';
-        boolean result=dfs(board,word,i+1,j,n,m,index+1)||
-                       dfs(board,word,i,j+1,n,m,index+1)|| 
-                       dfs(board,word,i-1,j,n,m,index+1)||
-                       dfs(board,word,i,j-1,n,m,index+1);
+        boolean result=dfs(board,word,index+1,i+1,j)||
+                       dfs(board,word,index+1,i-1,j)||
+                       dfs(board,word,index+1,i,j+1)||
+                       dfs(board,word,index+1,i,j-1);                     
         board[i][j]=temp;
         return result;
+
     }
 }
