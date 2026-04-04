485d1e4a1c30975ff08f116bae721871f5191924
Sushant2245 <guptasushant426@gmail.com>
2026-04-04 15:51:03 +0530

    Time: 388 ms (8.31%), Space: 42.8 MB (61.13%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    public boolean solve(char[][] board ){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char num='1';num<='9';num++){
+                        if(isSafe(board,num,i,j)){
+                            board[i][j]=num;
+                            if(solve(board))return true;
+                            else {
+                                board[i][j]='.';
+                            }
+                        }
+
+                    }
+                    return false;
+                }
+                
+            }
+        }
+        return true;
+    }
+    public boolean isSafe(char[][] board,char num,int row,int col){
+        for(int i=0;i<board.length;i++){
+            if(board[i][col]==num)return false;
+            if(board[row][i]==num)return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==num)return false;
+
+        }
+        return true;
+    }
+}
