faedac4f2e07a6e39cd7f4d48e7e20987d3c3523
Sushant2245 <guptasushant426@gmail.com>
2026-04-04 15:28:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,71 +1,59 @@
 class Solution {
-
-    // Check if queen can be placed at board[row][col]
-    boolean isSafe(int[][] board, int row, int col, int n) {
-
-        int r = row, c = col;
-
-        // upper-left diagonal
-        while (r >= 0 && c >= 0) {
-            if (board[r][c] == 1) return false;
-            r--; c--;
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result=new ArrayList<>();
+        int [][] board=new int[n][n];
+        solve(board,n,0,result);
+        return result;
+    }
+    List<String> construct(int[][] board,int n){
+        List<String> ls= new ArrayList<>();
+        for(int i=0;i<n;i++){
+            StringBuilder sb= new StringBuilder();
+            for(int j=0;j<n;j++){
+                if(board[i][j]==1)sb.append('Q');
+                else{
+                    sb.append('.');
+                }
+            }
+            ls.add(sb.toString());
         }
-
-        r = row; c = col;
-
-        // left row
-        while (c >= 0) {
-            if (board[r][c] == 1) return false;
+        return ls;
+    }
+    boolean isSafe(int row, int col,int n,int[][] board){
+        int r= row;
+        int c=col;
+        while(r>=0 && c>=0){
+            if(board[r][c]==1)return false;
+            r--;
             c--;
         }
-
-        r = row; c = col;
-
-        // lower-left diagonal
-        while (r < n && c >= 0) {
-            if (board[r][c] == 1) return false;
-            r++; c--;
+        r=row;
+        c=col;
+        while(c>=0){
+            if(board[r][c]==1)return false;
+            c--;
         }
-
+        r=row;
+        c=col;
+        while(r<n && c>=0){
+            if(board[r][c]==1)return false;
+            r++;
+            c--;
+        }
         return true;
     }
-
-    void solve(int[][] board, int col, int n, List<List<String>> res) {
-        if (col == n) {
-            res.add(constructBoard(board, n));
+    void solve(int[][] board ,int n, int col,List<List<String>> result){
+        if(col==n){
+            result.add(construct(board,n));
             return;
         }
-
-        for (int row = 0; row < n; row++) {
-            if (isSafe(board, row, col, n)) {
-                board[row][col] = 1;
-                solve(board, col + 1, n, res);
-                board[row][col] = 0; // backtrack
+        for(int row=0; row<n;row++){
+            if(isSafe(row,col,n,board)){
+                board[row][col]=1;
+                solve(board,n,col+1,result);
+               board[row][col]=0;
             }
+
         }
-    }
-
-    List<String> constructBoard(int[][] board, int n) {
-        List<String> temp = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 1)
-                    sb.append('Q');
-                else
-                    sb.append('.');
-            }
-            temp.add(sb.toString());
-        }
-
-        return temp;
-    }
-
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> res = new ArrayList<>();
-        int[][] board = new int[n][n];
-        solve(board, 0, n, res);
-        return res;
     }
 }
