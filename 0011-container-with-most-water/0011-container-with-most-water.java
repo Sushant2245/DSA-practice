b4da3d4ec0e3f83977979a36706de10fed1be72c
Sushant2245 <guptasushant426@gmail.com>
2026-04-05 12:48:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,42 +1,15 @@
-// class Solution {
-//     public int maxArea(int[] height) {
-//         int ans=0;
-//         int n=height.length;
-//         int left=0;
-//         int right=n-1;
-//         while(left<right){
-//             if(height[left]<height[right]){
-//                 ans=Math.max(ans,((left-right)*height[left]));
-//                 left++;
-//             }
-//             else{
-//                 ans=Math.max(ans,((left-right)*height[right]));
-//                 right++;
-//             }
-//         }
-//         return ans;
-//     }
-// }
 class Solution {
     public int maxArea(int[] height) {
-        int ans = 0;
-        int n = height.length;
-        int left = 0;
-        int right = n - 1;
-        
-        while (left < right) {
-            // Calculate the area with the current left and right
-            int currentArea = Math.min(height[left], height[right]) * (right - left);
-            ans = Math.max(ans, currentArea);
-            
-            // Move the pointer that points to the shorter line
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;  // This should be decrementing, not incrementing
-            }
+        int left=0;
+        int right=height.length-1;
+        int max=Integer.MIN_VALUE;
+        while(left<right){
+            int h=Math.min(height[left],height[right]);
+            int w=right-left;
+            max=Math.max(max,h*w);
+            if(height[left]<height[right])left++;
+            else right--;
         }
-        
-        return ans;
+        return max;
     }
 }
